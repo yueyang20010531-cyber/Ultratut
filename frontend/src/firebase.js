d78eb37e53850cc413bc575fffbe9a7c6b939fb9
yang yue <dannyyue@uw.edu>
2026-04-29 09:41:48 -0700

    key

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -5,14 +5,14 @@ import { getStorage } from "firebase/storage"
 import { getFunctions } from "firebase/functions"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD_UzmUSl3Zgjh60PK4ppZDbKTG8vHvhcY",
-  authDomain: "kmllk-7f416.firebaseapp.com",
-  databaseURL: "https://kmllk-7f416-default-rtdb.firebaseio.com",
-  projectId: "kmllk-7f416",
-  storageBucket: "kmllk-7f416.firebasestorage.app",
-  messagingSenderId: "392060493568",
-  appId: "1:392060493568:web:6895feb0ac687c07065de5",
-  measurementId: "G-FQ8VCE0775"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 }
 
 const app = initializeApp(firebaseConfig)
